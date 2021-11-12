080ff065132a21d8a1bad825a462cae29a2dea91
TY <1441212902@qq.com>
2021-11-13 03:32:01 +0800

    SingleActivity (partial)

--- app/src/main/java/com/baidu/paddle/lite/demo/yolo_detection/MainActivity.java
+++ app/src/main/java/com/baidu/paddle/lite/demo/yolo_detection/MainActivity.java
@@ -1,12 +1,10 @@
 package com.baidu.paddle.lite.demo.yolo_detection;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.app.Activity;
 import android.widget.Toast;
-import com.baidu.paddle.lite.demo.yolo_detection.TestActivity;
-import com.baidu.paddle.lite.demo.yolo_detection.R;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -27,11 +25,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private void initView() {
         single_btn = (View) findViewById(R.id.main_select_singlemode);
         vs_btn = (View) findViewById(R.id.main_select_vsmode);
-        logo=(View) findViewById(R.id.pplogo);
+        logo = (View) findViewById(R.id.pplogo);
         single_btn.setOnClickListener(this);
         vs_btn.setOnClickListener(this);
         logo.setOnClickListener(this);
-        clickedLogo=0;
+        clickedLogo = 0;
     }
 
     @Override
@@ -39,31 +37,33 @@ public class MainActivity extends Activity implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.main_select_singlemode:
                 Intent i = new Intent(MainActivity.this, SelectActivity.class);
-                i.putExtra("mode","single");
+                i.putExtra("mode", "single");
                 startActivity(i);
                 break;
             case R.id.main_select_vsmode:
                 Intent j = new Intent(MainActivity.this, SelectActivity.class);
-                j.putExtra("mode","vs");
+                j.putExtra("mode", "vs");
                 startActivity(j);
                 break;
             case R.id.pplogo:
                 clickedLogo++;
-                if(clickedLogo==7) {
+                if (clickedLogo == 7) {
                     clickedLogo = 0;
                     Intent k = new Intent(MainActivity.this, TestActivity.class);
                     startActivity(k);
-                }else if(clickedLogo>=2){
-                    String s="再点击"+String.valueOf(7-clickedLogo)+"次进入测试模式。";
+                } else if (clickedLogo >= 2) {
+                    String s = "再点击" + String.valueOf(7 - clickedLogo) + "次进入测试模式。";
                     showToast(s);
                 }
+            default:
         }
     }
-    private void showToast(String text){
-        if(myToast==null){
-            myToast = Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT);
+
+    private void showToast(String text) {
+        if (myToast == null) {
+            myToast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
             myToast.show();
-        }else{
+        } else {
             myToast.setText(text);
             myToast.show();
         }
